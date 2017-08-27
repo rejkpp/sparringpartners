0051de5f4313d9e9796263111853ca3f63edb147
rejkpp <rejkpp@users.noreply.github.com>
2017-08-27 12:23:30 +0200

    update.

--- ass/js/scripts.js
+++ ass/js/scripts.js
@@ -40,27 +40,10 @@ $(document).ready(function() {
 
 	/***************** Fancybox ******************/
 
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
 
 });
 
-$(document).ready(function() {
-	$("a.single_image").fancybox({
-		padding: 4,
-	});
-});
+
 
 /***************** Nav Transformicon ******************/
 
